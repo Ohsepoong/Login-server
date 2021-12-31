b3b5aea8a97d25e63dad25af4db6b89d46e3e65a
Ohsepoong <jazaknamu@gmail.com>
2021-12-31 19:27:13 +0900

    로그인을 요청하는 fetch생성

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -7,11 +7,22 @@ const loginBtn = document.querySelector("button");
 loginBtn.addEventListener("click", login);
 
 function login() {
-    // 요청하여 받은 값을 저장하는 코드 -> DOM을 이용하여 HTML객체를 저장
+    // front에서 USER가 요청으로 입력한 정보를  저장 -> DOM을 이용하여 HTML객체를 저장
     const req = {
         id : id.value,
         pw : pw.value
     };
-    console.log(req);
-    // 이 정보데이타를 값을 서버로 전달하는 코드
+    //console.log(req);
+    //console.log(JSON.stringify(req)); // 입력데이타의 문자열변환 코드
+    //console.log(req, JSON.stringify(req)); 
+    // {id: 'beryoza', pw: '1234'} '{"id":"beryoza","pw":"1234"}'
+
+    // 이 정보데이타를 서버로 전달하는 코드
+    fetch("/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(req)
+    })
 }
