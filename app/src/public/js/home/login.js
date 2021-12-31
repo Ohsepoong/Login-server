770a2c1d738cc07c006493f626e23e3b0132e297
Ohsepoong <jazaknamu@gmail.com>
2021-12-31 19:06:59 +0900

    DOM을 이용하여 HTML객체를 저장

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -1,4 +1,17 @@
 "use strict";
 
-console.log("hello world");
-console.log("hello world bye bye!");
+const id = document.querySelector("#id");
+const pw = document.querySelector("#pw");
+const loginBtn = document.querySelector("button");
+
+loginBtn.addEventListener("click", login);
+
+function login() {
+    // 요청하여 받은 값을 저장하는 코드 -> DOM을 이용하여 HTML객체를 저장
+    const req = {
+        id : id.value,
+        pw : pw.value
+    };
+    console.log(req);
+    // 이 정보데이타를 값을 서버로 전달하는 코드
+}
